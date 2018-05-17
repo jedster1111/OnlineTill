18566faa4c0808cd98cff2b921794e156da08c72
Jed <jedster1111@hotmail.co.uk>
2018-05-17 16:44:31 +0100

    Index hello world react test created

--- onlinetill/onlinetill/urls.py
+++ onlinetill/onlinetill/urls.py
@@ -18,7 +18,10 @@ Including another URLconf
 from django.urls import path, include
 from django.contrib import admin
 
+from . views import index
+
 urlpatterns = [
+    path('', index, name="home"),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
