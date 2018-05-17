c34f046e4e66d0566b7e6ca29880ce3e07ed9937
Jed <jedster1111@hotmail.co.uk>
2018-05-17 17:03:05 +0100

    Example hello world jsx page running with babel compiler

--- onlinetill/onlinetill/urls.py
+++ onlinetill/onlinetill/urls.py
@@ -18,10 +18,11 @@ Including another URLconf
 from django.urls import path, include
 from django.contrib import admin
 
-from . views import index
+from . views import index, jsx
 
 urlpatterns = [
     path('', index, name="home"),
+    path('jsx/', jsx, name="jsx"),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
--- onlinetill/onlinetill/views.py
+++ onlinetill/onlinetill/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index/index.html')
+
+def jsx(request):
+    return render(request, 'index/jsx.html')
