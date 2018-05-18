eb48612b7d84989c0690deece954d662e967d4b8
Jed <jedster1111@hotmail.co.uk>
2018-05-18 23:58:43 +0100

    Still not quite there getting react to work, starting a todo tutorial

--- onlinetill/onlinetill/urls.py
+++ onlinetill/onlinetill/urls.py
@@ -21,9 +21,8 @@ from django.contrib import admin
 from . views import index, jsx, jsx2
 
 urlpatterns = [
-    path('', index, name="home"),
+    path('api/', include('api.urls')),
     path('jsx/', jsx, name="jsx"),
     path('jsx2/', jsx2, name="jsx2"),
-    path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
