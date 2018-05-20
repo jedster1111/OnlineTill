199ded21fc5faf4424fcbc1a641062daf086f937
Jed <jedster1111@hotmail.co.uk>
2018-05-20 19:45:01 +0100

    Start of tictactoe tutorial

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Game from './Tictactoe';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <div>
+        <App />
+        <Game />
+    </div>
+    , document.getElementById('root'));
 registerServiceWorker();
