3e5e919a851177f7f5be0e317ab78cb87a127ece
Jed <jedster1111@hotmail.co.uk>
2018-05-28 04:35:55 +0100

    added celsius fahrenheit converter function, just about to move state up

--- frontend/src/Tempcalculator.js
+++ frontend/src/Tempcalculator.js
@@ -7,9 +7,30 @@ function BoilingVerdict(props) {
     return <p>The water would not boil.</p>;
 }
 
+function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
 
+function toFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+}
 
-class Calculator extends React.Component{
+function tryConvert(temperature, convert) {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+        return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+}
+
+const scaleNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit',
+};
+
+class TemperatureInput extends React.Component{
     constructor(props){
         super(props);
         this.handleChange = this.handleChange.bind(this);
@@ -26,20 +47,28 @@ class Calculator extends React.Component{
 
     render(){
         const temperature = this.state.temperature;
+        const scale = this.props.scale;
         return(
             <fieldset>
-                <legend>Enter temperature in Celsius:</legend>
+                <legend>Enter temperature in {scaleNames[scale]}:</legend>
                 <input
                     value={temperature}
                     onChange={this.handleChange} />
-
-                <BoilingVerdict
-                    celsius={parseFloat(temperature)} />
-
             </fieldset>
         )
     }
 
+}
+
+class Calculator extends React.Component {
+    render() {
+        return(
+            <div>
+                <TemperatureInput scale="c" />
+                <TemperatureInput scale="f" />
+            </div>
+        );
+    }
 }
 
 
